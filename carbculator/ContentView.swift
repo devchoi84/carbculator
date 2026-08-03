044e25a2ab3fe6f4e214ac02df6cb4249b854dc8
Martin.garrix <martin.garrix@kakaoent.com>
2026-08-03 09:53:45 +0900

    Initial Commit

---
+++ carbculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  carbculator
+//
+//  Created by martin-garrix-L on 8/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ carbculator/carbculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  carbculatorApp.swift
+//  carbculator
+//
+//  Created by martin-garrix-L on 8/3/26.
+//
+
+import SwiftUI
+
+@main
+struct carbculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
